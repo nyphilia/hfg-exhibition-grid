08217e7d42450581d784ffe5ecb7d8d2adbbabf4
Celina Wintersinger <41753886+nyphilia@users.noreply.github.com>
2023-07-12 00:39:37 +0200

    initial upload

---
+++ exhibition-grid/gruppe-a/showcase_html/content/content.js
@@ -0,0 +1,5 @@
+// Array of the content folders
+var contentFolders = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+
+// Determines how often the kiosk should reset itself (in ms).
+var refreshInterval = 60000
