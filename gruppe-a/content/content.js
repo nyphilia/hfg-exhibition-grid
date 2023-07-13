c77046b67abe72449f8983ccf2d7627da58be0d2
Celina Wintersinger <41753886+nyphilia@users.noreply.github.com>
2023-07-13 14:41:21 +0200

    fixes

--- gruppe-a/content/content.js
+++ gruppe-a/content/content.js
@@ -1,5 +1,5 @@
 // Array of the content folders
-var contentFolders = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12", "14"];
+var contentFolders = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12", "13", "14"];
 
 // Determines how often the kiosk should reset itself (in ms).
-var refreshInterval = 60000
+var refreshInterval = 120000
--- gruppe-b/content/content.js
+++ gruppe-b/content/content.js
@@ -2,4 +2,4 @@
 var contentFolders = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "13", "14", "15", "16", "17"];
 
 // Determines how often the kiosk should reset itself (in ms).
-var refreshInterval = 60000
+var refreshInterval = 120000
